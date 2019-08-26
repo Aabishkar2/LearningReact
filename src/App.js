9fc31f60bbec3996c7118bd8f41094dc0f71f495
Aabishkar Wagle <aabishkar2@gmail.com>
2019-08-26 12:27:52 +0545

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React, { Component } from 'react';
-import Todos from './components/todos/Todos'
-import logo from './logo.svg';
+import Header from './components/header/Header'
 import './App.css';
 
 class App extends Component {
 
-  state = {
-    todos : [
-      {
-        id: 1,
-        title: "Peaky Blinders",
-        status: "watched"
-      },
-      {
-        id: 2,
-        title: "Narcos",
-        status: "watched"
-      },
-      {
-        id: 3,
-        title: "Breaking Bad",
-        status: "not-watched"
-      },
-    ]
-  }
-
   render() {
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} />
-        <h3>Nepal</h3>
-      </div>
+      <Header />
     );
   }
  
--- src/components/todos/TodosItem.js
+++ src/components/todos/TodosItem.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
 
 class TodosItem extends Component {
+
+    giveStyle = () => {
+        return {
+            textDecoration: this.props.todo.status === 'watched' ? 'line-through' : 'none'
+        }
+    }
+
     render() {
         return (
             <div>
-                <div>
+                <div style={this.giveStyle()}>
                     <h3> {this.props.todo.title} </h3>
                 </div>
             </div>
